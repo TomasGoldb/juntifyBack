795ec2c517c5cf123a42fa8522bca708b2d1e3ad
Android Studio <devandroid@ort>
2025-07-14 08:53:28 -0300

    fixed

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,16 +5,6 @@ import { authenticateToken } from '../middlewares/authentication-middleware.js';
 const router = express.Router();
 const userService = new UserService();
 
-// Registro de usuario
-router.post('/registro', async (req, res) => {
-  await userService.registro(req, res);
-});
-
-// Login de usuario
-router.post('/login', async (req, res) => {
-  await userService.login(req, res);
-});
-
 // Obtener amigos del usuario
 router.get('/amigos/:userId', authenticateToken, async (req, res) => {
   await userService.obtenerAmigos(req, res);
