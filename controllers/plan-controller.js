097d47ae1589366676e51857ee0647dfff76212c
TomasGoldb <48588022@est.ort.edu.ar>
2025-08-15 23:38:09 -0300

    debug logs

--- controllers/plan-controller.js
+++ controllers/plan-controller.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PlanService } from '../services/plan-service.js';
+import { authenticateToken } from '../middlewares/authentication-middleware.js';
 
 const router = express.Router();
 const planService = new PlanService();
@@ -7,7 +8,7 @@ const planService = new PlanService();
 router.post('/crear', (req, res) => planService.crearPlan(req, res));
 router.post('/aceptar-invitacion', (req, res) => planService.aceptarInvitacion(req, res));
 router.post('/declinar-invitacion', (req, res) => planService.declinarInvitacion(req, res));
-router.post('/:idPlan/iniciar', (req, res) => planService.iniciarPlan(req, res));
+router.post('/:idPlan/iniciar', authenticateToken, (req, res) => planService.iniciarPlan(req, res));
 
 router.get('/usuario/:userId', (req, res) => planService.planesDeUsuario(req, res));
 router.get('/usuario/:userId/mas', (req, res) => planService.cargarMasPlanes(req, res));
